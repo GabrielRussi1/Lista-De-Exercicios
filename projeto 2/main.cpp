b203c7c952624e1b857e3779865be80ab276f85f
Gabriel Russi <ggab.russi@gmail.com>
2025-03-12 16:58:46 -0300

    Add files via upload

---
+++ projeto 2/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float numero, dobro;
+
+    // Solicita ao usuário um número real
+    printf("Digite um número: ");
+    scanf("%f", &numero);
+
+    // Calcula o dobro do número
+    dobro = numero * 2;
+
+    // Exibe o resultado
+    printf("O dobro de %.2f é %.2f\n", numero, dobro);
+
+    return 0;
+}
