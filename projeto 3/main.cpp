ab0675f0551377eb90a51e2281a01415f3dfb673
Gabriel Russi <ggab.russi@gmail.com>
2025-03-12 17:06:52 -0300

    Add files via upload

---
+++ projeto 3/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float distancia, tempo, velocidade_media;
+
+    // Solicita ao usuário a distância percorrida
+    printf("Digite a distância percorrida (em km): ");
+    scanf("%f", &distancia);
+
+    // Solicita ao usuário o tempo gasto
+    printf("Digite o tempo gasto (em horas): ");
+    scanf("%f", &tempo);
+
+    // Calcula a velocidade média
+    velocidade_media = distancia / tempo;
+
+    // Exibe o resultado
+    printf("A velocidade média é %.2f km/h\n", velocidade_media);
+
+    return 0;
+}
