46cbd20396a589c6f11e8d9ccaca53a54e0ea079
Gabriel Russi <ggab.russi@gmail.com>
2025-04-08 15:47:33 -0300

    Update main.cpp

--- projeto 3/main.cpp
+++ projeto 3/main.cpp
@@ -3,19 +3,19 @@
 int main() {
     float distancia, tempo, velocidade_media;
 
-    // Solicita ao usu·rio a dist‚ncia percorrida
-    printf("Digite a dist‚ncia percorrida (em km): ");
+    // Solicita ao usu√°rio a dist√¢ncia percorrida, complicado essa
+    printf("Digite a dist√¢ncia percorrida (em km): ");
     scanf("%f", &distancia);
 
-    // Solicita ao usu·rio o tempo gasto
+    // Solicita ao usu√°rio o tempo gasto
     printf("Digite o tempo gasto (em horas): ");
     scanf("%f", &tempo);
 
-    // Calcula a velocidade mÈdia
+    // Calcula a velocidade m√©dia
     velocidade_media = distancia / tempo;
 
-    // Exibe o resultado
-    printf("A velocidade mÈdia È %.2f km/h\n", velocidade_media);
+    // resultado
+    printf("A velocidade m√©dia √© %.2f km/h\n", velocidade_media);
 
     return 0;
 }
